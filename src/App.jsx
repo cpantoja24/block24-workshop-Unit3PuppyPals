75958d37039a900b86736abb8139b486ecc59669
Cheyenne Pantoja <Cheyenne@MBP-5000.frontiernet.net>
2023-12-08 13:27:07 -0800

    mapped over puppies

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
+  // Notice that you have passed the puppyList array from the data.js file as the default value.
+  // Important: Always remember to pass your useState
+  const [puppies, setPuppies] = useState(puppyList);
 
+  // does the same as console.log(puppyList)
+  console.log(puppies);
 
   return (
+    // use {} to escape into/use JavaScript
     <>
-      <div>
-       
+      <div className='App'>
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>
+            {puppy.name}</p>
+        })};
       </div>
-      
     </>
   )
 }
