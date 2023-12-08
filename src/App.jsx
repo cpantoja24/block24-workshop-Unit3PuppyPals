bf2e37a3982e7477cb02eb4013a705fd8e1e4e79
Cheyenne Pantoja <Cheyenne@MBP-5000.frontiernet.net>
2023-12-08 14:37:17 -0800

    render puppy details

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import {puppyList} from './data.js'
+import { puppyList } from './data.js'
 
 function App() {
   // Notice that you have passed the puppyList array from the data.js file as the default value.
   // Important: Always remember to pass your useState
   const [puppies, setPuppies] = useState(puppyList);
+
+  const [featPupId, setfeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
 
   // does the same as console.log(puppyList)
   console.log(puppies);
@@ -14,10 +18,24 @@ function App() {
     // use {} to escape into/use JavaScript
     <>
       <div className='App'>
+
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>
-            {puppy.name}</p>
-        })};
+
+          return (<p onClick={() => {
+            setfeatPupId(puppy.id)
+          }} key={puppy.id}>
+            {puppy.name}</p>)
+        })}
+
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   )
